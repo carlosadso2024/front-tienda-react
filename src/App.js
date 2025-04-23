94450cc30ad4897ab78867eda947f2cdc55b6da9
carlosadso2024 <carlosmario.a@hotmail.com>
2025-04-23 16:11:47 -0500

    corrigiendo deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
         <Routes>
